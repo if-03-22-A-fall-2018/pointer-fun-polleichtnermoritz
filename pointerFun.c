098fc9c1d4ab4f98f31c0ae9bf7eb71cb66a28f1
polleichtnermoritz <moritz.polleichtner@outlook.com>
2018-10-13 12:58:03 +0200

    done

---
+++ pointerFun.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+void print_integers(int int_value, int* int_pointer);
+void change_integers(int int_value, int* int_pointer);
+
+int main(int argc, char const *argv[]) {
+  int int_value=56;
+  int* int_pointer=&int_value;
+
+  print_integers(int_value, int_pointer);
+  change_integers(int_value, int_pointer);
+  print_integers(int_value, int_pointer);
+
+  return 0;
+}
+
+void print_integers(int int_value, int* int_pointer)
+{
+  printf("Got an integer value %d and an address to an integer with value %d\n",int_value, *int_pointer );
+}
+
+void change_integers(int int_value, int* int_pointer)
+{
+  int_value=45;
+  *int_pointer=24;
+}
